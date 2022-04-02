409769dbd8ac02e2e70fe643b651b1bc7d58b81d
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-04-02 09:50:07 +0200

    Get computer move

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-let computerMove = "kamień";
-let playerMove = "papier";
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+/*let computerMove = "kamień";*/
+console.log("Wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else {
+  computerMove = "nożyce";
+}
+
+let playerMove = "kamień";
 
 printMessage(
   "Zagrałem " +
